bc520310cc4046021444505253f8f84a40ebc033
truqquan <141379238+truqquan@users.noreply.github.com>
2025-02-15 15:38:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -35,4 +35,40 @@ function eraseText(){
         setTimeout(typeWriter, 500)
     }
 }
-window.onload = typeWriter
+
+// Typewriter Effect for "THÚY AN"
+const texts2 = [
+    "NGUYỄN HOÀNG THỤY AN",
+    "TRẦN BẢO CHÂU",
+    "VÕ HỒNG THI",
+    "TRẦN THỊ HUYỀN TRANG"
+]
+let textIndex2 = 0;
+let charcterIndex2 = 0;
+const textElements2 = document.querySelector(".typewriter-text-2");
+
+function typeWriter2() {
+    if (charcterIndex2 < texts2[textIndex2].length) {
+        textElements2.innerHTML += texts2[textIndex2].charAt(charcterIndex2);
+        charcterIndex2++;
+        setTimeout(typeWriter2, speed);
+    } else {
+        setTimeout(eraseText2, 1000)
+    }
+}
+
+function eraseText2() {
+    if (textElements2.innerHTML.length > 0) {
+        textElements2.innerHTML = textElements2.innerHTML.slice(0, -1);
+        setTimeout(eraseText2, 50)
+    } else {
+        textIndex2 = (textIndex2 + 1) % texts2.length;
+        charcterIndex2 = 0;
+        setTimeout(typeWriter2, 500)
+    }
+}
+
+window.onload = () => {
+    typeWriter();
+    typeWriter2(); // Start the second typewriter effect
+}
